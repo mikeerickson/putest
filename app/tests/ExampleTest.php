f3ad4e523e8bdb8a593923475e8f8e1305c9bec9
Mike Erickson <codedungeon@gmail.com>
2014-01-20 16:35:29 -0800

    fixed example test

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -14,27 +14,6 @@ class ExampleTest extends TestCase {
         $this->assertFalse(false);
     }
 
-//    public function testToObject()
-//    {
-//        $arr = [
-//            'id' => 1,
-//            'name' => 'Michael',
-//            'email' => 'michael@clouddueling.com',
-//            'card' => [
-//                'id' => 2
-//            ]
-//        ];
-//
-//        $obj = to_object($arr);
-//
-//        $this->assertObjectHasAttribute('id', $obj);
-//        $this->assertEquals($obj->id, 1);
-//        $this->assertObjectHasAttribute('name', $obj);
-//
-//        $this->assertObjectHasAttribute('id', $obj->card);
-//        $this->assertEquals($obj->card->id, 2);
-//    }
-//
     public function testToTimezone()
     {
         $date = "2013-12-31 13:00:00";
